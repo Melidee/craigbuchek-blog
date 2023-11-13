00ab905c0eaf9c5abe2a385aa010d63983d33f7e
Melidee <86878748+Melidee@users.noreply.github.com>
2023-11-13 01:36:41 -0600

    files updated with relative paths

--- wp-login.php
+++ wp-login.php
@@ -1,43 +1,41 @@
 <!DOCTYPE html>
-	<!--[if IE 8]>
+
+<!--[if IE 8]>
 		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
 	<![endif]-->
-	<!--[if !(IE 8) ]><!-->
-		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
-	<!--<![endif]-->
-	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Craig Buchek&#039;s Blog &lsaquo; Log In</title>
-	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='./wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.7.2' type='text/css' media='all' />
-<meta name='robots' content='noindex,follow' />
-	<meta name="viewport" content="width=device-width" />
-		</head>
-	<body class="login login-action-login wp-core-ui  locale-en-us">
-		<div id="login">
-		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Craig Buchek&#039;s Blog</a></h1>
-	
-<form name="loginform" id="loginform" action="./wp-login.php" method="post">
-	<p>
-		<label for="user_login">Username or Email Address<br />
-		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
-	</p>
-	<p>
-		<label for="user_pass">Password<br />
-		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
-	</p>
-		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
-	<p class="submit">
-		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="./wp-admin/" />
-		<input type="hidden" name="testcookie" value="1" />
-	</p>
-</form>
-
-<p id="nav">
-	<a href="./wp-login.php?action=lostpassword">Lost your password?</a>
+<!--[if !(IE 8) ]><!-->
+<html lang="en-US" xmlns="http://www.w3.org/1999/xhtml">
+<!--<![endif]-->
+<head>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+<title>Craig Buchek's Blog ‹ Log In</title>
+<link href="//s.w.org" rel="dns-prefetch"/>
+<link href="../wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.7.2" media="all" rel="stylesheet" type="text/css"/>
+<meta content="noindex,follow" name="robots"/>
+<meta content="width=device-width" name="viewport"/>
+</head>
+<body class="login login-action-login wp-core-ui locale-en-us">
+<div id="login">
+<h1><a href="https://wordpress.org/" tabindex="-1" title="Powered by WordPress">Craig Buchek's Blog</a></h1>
+<form action="http://blog.craigbuchek.com/wp-login.php" id="loginform" method="post" name="loginform">
+<p>
+<label for="user_login">Username or Email Address<br/>
+<input class="input" id="user_login" name="log" size="20" type="text" value=""/></label>
 </p>
-
+<p>
+<label for="user_pass">Password<br/>
+<input class="input" id="user_pass" name="pwd" size="20" type="password" value=""/></label>
+</p>
+<p class="forgetmenot"><label for="rememberme"><input id="rememberme" name="rememberme" type="checkbox" value="forever"/> Remember Me</label></p>
+<p class="submit">
+<input class="button button-primary button-large" id="wp-submit" name="wp-submit" type="submit" value="Log In"/>
+<input name="redirect_to" type="hidden" value="http://blog.craigbuchek.com/wp-admin/"/>
+<input name="testcookie" type="hidden" value="1"/>
+</p>
+</form>
+<p id="nav">
+<a href="wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
 <script type="text/javascript">
 function wp_attempt_focus(){
 setTimeout( function(){ try{
@@ -51,13 +49,8 @@ d.select();
 wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
-
-	<p id="backtoblog"><a href="./">&larr; Back to Craig Buchek&#039;s Blog</a></p>
-	
-	</div>
-
-	
-		<div class="clear"></div>
-	</body>
-	</html>
-	
+<p id="backtoblog"><a href="">← Back to Craig Buchek's Blog</a></p>
+</div>
+<div class="clear"></div>
+</body>
+</html>
